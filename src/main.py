51f5f7b125b32bc2fb9b140cd3c6966d5aec5a4a
Gineth <ginysp@hotmail.com>
2021-04-07 03:08:04 +0000

    dkkdd

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Films
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():  
+    
+    return jsonify(People.getAllPeople()), 200
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():  
+    
+    return jsonify(Planets.getAllPlanets()), 200
+
+@app.route('/films', methods=['GET'])
+def handle_films():  
+    
+    return jsonify(Films.getAllFilms()), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def handle_peopleid(id):  
+    
+    return jsonify(People.getPerson(id)), 200
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -10,21 +10,65 @@ class User(db.Model):
     lastname = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(15), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email           
+        }
+
+
 class People(db.Model):
     __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(28), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name           
+        }
+    
+    def getAllPeople():
+        all_people = People.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return all_people
+    
+    def getPerson(id):
+        person = People.query.get(id)
+        return person.serialize()
+
+
 class Planets(db.Model):
     __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(28), nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name           
+        }
+    def getAllPlanets():
+        all_planets = Planets.query.all()
+        all_planets = list(map(lambda x: x.serialize(), all_planets))
+        return all_planets
+
+class Films(db.Model):
+    __tablename__ = 'films'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(28), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "title": self.title           
         }
+    def getAllFilms():
+        all_films = Films.query.all()
+        all_films = list(map(lambda x: x.serialize(), all_films))
+        return all_films
+    
